d3931b69761007e56b0ab635db52cbe61ddb00d9
Arun Kumar Pyasi <expertaruncorp@gmail.com>
2018-10-27 01:15:09 +0545

    added serializer and cleanup

--- api/api/models.py
+++ api/api/models.py
@@ -9,10 +9,9 @@ from django.db import models
 class Notices(models.Model):
     title = models.TextField(max_length=100)
     notice = models.TextField(max_length=None)
-    expiry = models.DateTimeField()
+    date_expiry = models.DateTimeField()
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
-    date_deleted = models.DateTimeField(auto_now=True)
 
     def __str__ (self):
         """Return a human readable representation of the model instance."""
